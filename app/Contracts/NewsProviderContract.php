5e3f1d79f57bb6cc3bd709ae14239f3eb48de982
Ahmed Nagi <ahmedflnagi@gmail.com>
2025-03-12 23:05:21 +0200

    Update NewsProviderContract to use NewsProviderOptions for fetching news

--- app/Contracts/NewsProviderContract.php
+++ app/Contracts/NewsProviderContract.php
@@ -2,14 +2,14 @@
 
 namespace App\Contracts;
 
+use App\Support\NewsProviderOptions;
+
 interface NewsProviderContract
 {
     /**
      * Fetch the latest news from the provider.
      *
-     * @param  string  $fromDate  Minimum date and time for the oldest article allowed.
-     * @param  string  $toDate  Maximum date and time for the newest article allowed.
-     * @param  int  $limit  Maximum number of articles to fetch.
+     * @param  NewsProviderOptions  $options  Options for fetching news.
      */
-    public function fetchLatestNewsCursor(string $fromDate, string $toDate, int $limit = 50): \Illuminate\Support\LazyCollection;
+    public function fetchLatestNewsCursor(NewsProviderOptions $options): \Illuminate\Support\LazyCollection;
 }
