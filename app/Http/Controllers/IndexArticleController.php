a754f0a928acfece987e50dd2393e68f38f4fbbc
Ahmed Nagi <ahmedflnagi@gmail.com>
2025-03-12 20:40:16 +0200

    start fresh

--- app/Http/Controllers/IndexArticleController.php
+++ app/Http/Controllers/IndexArticleController.php
@@ -2,27 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\ArticleResource;
-use App\Models\Article;
 use Illuminate\Http\Request;
-use Spatie\QueryBuilder\AllowedFilter;
-use Spatie\QueryBuilder\QueryBuilder;
 
 class IndexArticleController extends Controller
 {
-    public function __invoke(Request $request)
-    {
-        return ArticleResource::collection(
-            QueryBuilder::for(Article::class)
-                ->allowedFilters([
-                    AllowedFilter::partial('title'),
-                    AllowedFilter::partial('category'),
-                    AllowedFilter::partial('source'),
-                    AllowedFilter::scope('date'),
-                ])
-                ->allowedSorts('title', 'published_at')
-                ->defaultSort('published_at')
-                ->paginate()
-        );
-    }
+    public function __invoke(Request $request) {}
 }
