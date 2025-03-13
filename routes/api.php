12f9f696dd473e7dbccfc668ba43cf7b1610c716
Ahmed Nagi <ahmedflnagi@gmail.com>
2025-03-13 17:46:58 +0200

    Add user preferences routes to API

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,11 @@
 <?php
 
 use App\Http\Controllers\IndexArticleController;
+use App\Http\Controllers\UserPreferenceController;
 use Illuminate\Support\Facades\Route;
 
 // Article
 Route::get('articles', IndexArticleController::class);
 
 // User Preferences
-// Route::get('user-preferences', IndexUserPreferenceController::class);
-// Route::post('user-preferences', StoreUserPreferenceController::class);
-
-// Route::get('article-categories', IndexArticleCategoryController::class);
-
-// Route::get('sources', IndexSourceController::class);
+Route::resource('user-preferences', UserPreferenceController::class);
