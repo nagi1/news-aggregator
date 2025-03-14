52255756ab4d4a0da88af847ab4d37904cebee2a
Ahmed Nagi <ahmedflnagi@gmail.com>
2025-03-14 16:15:34 +0200

    Add scheduled task to fetch news from providers every six hours

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
+use App\Enums\NewsProviderEnum;
+use Illuminate\Support\Facades\Artisan;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,6 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(function () {
+            foreach (NewsProviderEnum::cases() as $provider) {
+                Artisan::call('fetch:news', ['provider' => $provider->value]);
+            }
+        })->everySixHours();
+
+    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
--- routes/console.php
+++ routes/console.php
@@ -3,9 +3,3 @@
 use App\Enums\NewsProviderEnum;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Schedule::call(function () {
-    foreach (NewsProviderEnum::cases() as $provider) {
-        Artisan::call('fetch:news', ['provider' => $provider->value]);
-    }
-})->everySixHours();
